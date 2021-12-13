ea167945800fe7b0342782aa3155c7dd637bd3c7
dccif <heyujieboy@163.com>
2021-12-12 23:32:26 -0500

    try to add c# file

--- src/Trie/TrieNode.kt
+++ src/Trie/TrieNode.kt
@@ -1,3 +1,76 @@
 package Trie
 
-class TrieNode(val pass: Int = 0, val end: Int = 0, val nexts: Array<TrieNode> = Array(26) { TrieNode() })
+class TrieNode(var pass: Int = 0, var end: Int = 0, var nexts: Array<TrieNode?> = Array(26) { TrieNode() })
+
+class Trie(var root: TrieNode = TrieNode()) {
+    fun insert(word: String) {
+        if (word == null) {
+            return
+        }
+        val chs = word.toCharArray()
+        var node = root
+        node.pass++
+        var index = 0
+        for (i in chs.indices) {
+            index = chs[i] - 'a'
+            if (node.nexts[index] == null) {
+                node.nexts[index] = TrieNode()
+            }
+            node = node.nexts[index]!!
+            node.pass++
+        }
+        node.end++
+    }
+
+    fun delete(word: String) {
+        if (search(word) != 0) {
+            val chs = word.toCharArray()
+            var node = root
+            node.pass--
+            var index = 0
+            for (i in chs.indices) {
+                index = chs[i] - 'a'
+                if (--node.nexts[index]!!.pass == 0) {
+                    node.nexts[index] = null
+                    return
+                }
+                node = node.nexts[index]!!
+            }
+            node.end--
+        }
+    }
+
+    fun search(word: String): Int {
+        if (word == null) {
+            return 0
+        }
+        val chs = word.toCharArray()
+        var node = root
+        var index = 0
+        for (i in chs.indices) {
+            index = chs[i] - 'a'
+            if (node.nexts[index] == null) {
+                return 0
+            }
+            node = node.nexts[index]!!
+        }
+        return node.end
+    }
+
+    fun prefixNumber(pre: String): Int {
+        if (pre == null) {
+            return 0
+        }
+        val chs = pre.toCharArray()
+        var node = root
+        var index = 0
+        for (i in chs.indices) {
+            index = chs[i] - 'a'
+            if (node.nexts[index] == null) {
+                return 0
+            }
+            node = node.nexts[index]!!
+        }
+        return node.pass
+    }
+}
---
+++ src/Trie/TrieTree.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+public class TrieTree
+{
+    public class TrieNode
+    {
+        public int pass;
+        public int end;
+        public TrieNode[] nexts;
+
+        public TrieNode()
+        {
+            pass = 0;
+            end = 0;
+            nexts = new TrieNode[26];
+        }
+    }
+    public class Trie
+    {
+        private TrieNode root;
+        public Trie()
+        {
+            root = new TrieNode();
+        }
+
+        public void insert(String word)
+        {
+            if(word == null)
+            {
+                return;
+            }
+            char[] chs = word.ToCharArray();
+            TrieNode node = root;
+            node.pass++;
+            int index = 0;
+            for(int i = 0; i < chs.Length; i++)
+            {
+                index = chs[i] - 'a';
+                if(node.nexts[index] == null)
+                {
+                    node.nexts[index] = new TrieNode();
+                }
+                node = node.nexts[index];
+                node.pass++;
+            }
+            node.end++;
+        }
+
+        public void delete(String word)
+        {
+            if (search(word) != 0)
+            {
+                char[] chs = word.ToCharArray();
+                TrieNode node = root;
+                node.pass--;
+                int index = 0;
+                for(int i = 0;i< chs.Length; i++)
+                {
+                    index = chs[i] - 'a';
+                    if(--node.nexts[index].pass == 0)
+                    {
+                        node.nexts[index] = null;
+                        return;
+                    }
+                    node = node.nexts[index];
+                }
+                node.end--;
+            }
+        }
+
+        public int search(String word)
+        {
+            if (word == null)
+            {
+                return 0;
+            }
+            char[] chs = word.ToCharArray();
+            TrieNode node = root;
+            int index = 0;
+            for(int i = 0; i < chs.Length; i++)
+            {
+                index = chs[i] - 'a';
+                if(node.nexts[index] == null)
+                {
+                    return 0;
+                }
+                node =node.nexts[index];
+            }
+            return node.end;
+        }
+
+        public int prefixNumber(String pre)
+        {
+
+            if (pre== null)
+            {
+                return 0;
+            }
+            char[] chs = pre.ToCharArray();
+            TrieNode node = root;
+            int index = 0;
+            for (int i = 0; i < chs.Length; i++)
+            {
+                index = chs[i] - 'a';
+                if (node.nexts[index] == null)
+                {
+                    return 0;
+                }
+                node = node.nexts[index];
+            }
+            return node.pass;
+        }
+    }
+    public static void Main(String[] args)
+    {
+        System.Diagnostics.Debug.WriteLine("This is in debug 2");
+        Console.ReadLine();
+    }
+}
