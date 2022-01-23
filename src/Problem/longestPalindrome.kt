07d6e179391864002fd9baac9f6e529a97220ad0
dccif <heyujieboy@163.com>
2022-01-22 23:23:44 -0500

    Zigzag Conversion

--- src/Problem/longestPalindrome.kt
+++ src/Problem/longestPalindrome.kt
@@ -1,5 +1,6 @@
 package Problem
 
+
 class longestPalindrome {
     fun longestPalindrome(s: String?): String? {
         if (s == null || s.length < 1) return ""
@@ -26,7 +27,21 @@ class longestPalindrome {
         }
         return R - L - 1
     }
-
+    fun convert(s: String, numRows: Int): String? {
+        if (numRows == 1) return s
+        val rows: MutableList<StringBuilder> = ArrayList()
+        for (i in 0 until Math.min(numRows, s.length)) rows.add(StringBuilder())
+        var curRow = 0
+        var goingDown = false
+        for (c in s.toCharArray()) {
+            rows[curRow].append(c)
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown
+            curRow += if (goingDown) 1 else -1
+        }
+        val ret = StringBuilder()
+        for (row in rows) ret.append(row)
+        return ret.toString()
+    }
 }
 
 fun main() {
@@ -35,4 +50,5 @@ fun main() {
     var test = longestPalindrome()
 
     println(test.longestPalindrome(a))
+    println(test.convert(a,3))
 }
