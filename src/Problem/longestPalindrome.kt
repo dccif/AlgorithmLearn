be6aabb718b78261677ed0f2d8cfac56ef8b1ec4
dccif <heyujieboy@163.com>
2022-01-23 00:02:10 -0500

    Regular Expression Matching

--- src/Problem/longestPalindrome.kt
+++ src/Problem/longestPalindrome.kt
@@ -108,6 +108,18 @@ class longestPalindrome {
 
         return x == revertnumbe || x == revertnumbe / 10
     }
+
+    fun isMatch(text: String, pattern: String): Boolean {
+        if (pattern.isEmpty()) return text.isEmpty()
+        val first_match = !text.isEmpty() &&
+                (pattern[0] == text[0] || pattern[0] == '.')
+        return if (pattern.length >= 2 && pattern[1] == '*') {
+            isMatch(text, pattern.substring(2)) ||
+                    first_match && isMatch(text.substring(1), pattern)
+        } else {
+            first_match && isMatch(text.substring(1), pattern.substring(1))
+        }
+    }
 }
 
 fun main() {
